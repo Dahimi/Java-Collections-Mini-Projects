e54b21362cbf540c28d2c74aca8a7e855a8dfaf7
DahimiSoufiane <DAHIMI Soufiane>
2020-05-16 20:27:18 +0000

    starting a new Big task : first we inherit the abstractList class and overrinding abstract and with 'element' index ' methods

--- AbstractList Class/src/abstracltListClass/CustomTree.java
+++ AbstractList Class/src/abstracltListClass/CustomTree.java
@@ -2,7 +2,38 @@ package abstracltListClass;
 
 import java.io.Serializable;
 import java.util.AbstractList;
+import java.util.*;
+public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
 
-public class CustomTree extends AbstractList<E> implements Cloneable, Serializable {
+	@Override
+	public String get(int arg0) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public String set(int index, String element) {
+		throw new UnsupportedOperationException();
+	}
+	public void add(int index, String element) {
+		throw new UnsupportedOperationException();
+	}
+	public String remove(int index) {
+		throw new UnsupportedOperationException();
+	}
+	public List<String> subList(int fromIndex, int toIndex){
+		throw new UnsupportedOperationException();
+	}
+	protected void removeRange(int fromIndex, int toIndex) {
+		throw new UnsupportedOperationException();
+	}
+	public boolean addAll(int index, Collection<? extends String> c) {
+		throw new UnsupportedOperationException();
+	}
 
 }
