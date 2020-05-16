7d04d470fbeb241f45d033080879b8c4d46da350
DahimiSoufiane <DAHIMI Soufiane>
2020-05-16 19:13:01 +0000

    starting a new Big task : first we inherit the abstractList class and overrinding abstract and with 'element' index ' methods

---
+++ AbstractList Class/src/abstracltListClass/CustomTree.java
@@ -0,0 +1,8 @@
+package abstracltListClass;
+
+import java.io.Serializable;
+import java.util.AbstractList;
+
+public class CustomTree extends AbstractList<E> implements Cloneable, Serializable {
+
+}
