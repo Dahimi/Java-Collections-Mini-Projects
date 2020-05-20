1ede01299139106511963b496b356bb3bd205a66
DahimiSoufiane <DAHIMI Soufiane>
2020-05-20 12:38:14 +0000

    start a new mini-project , build the main classes : Tetris , gamePiece , Field

---
+++ Tetris game/src/game/Tetris.java
@@ -0,0 +1,23 @@
+package game;
+
+public class Tetris {
+	private GamePiece gamePiece ;
+	private Field field ;
+	public GamePiece getGamePiece() {
+		return gamePiece;
+	}
+	public void setGamePiece(GamePiece gamePiece) {
+		this.gamePiece = gamePiece;
+	}
+	public Field getField() {
+		return field;
+	}
+	public void setField(Field field) {
+		this.field = field;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
